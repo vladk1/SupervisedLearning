1051bf1e63bbd8e703ff8e1ced661390ed528683
Джон <kolesnik-vlad@mail.ru>
2016-01-13 03:46:54 +0000

    q5 Major data fix.

--- ex5_tuning_reg_parameter.m
+++ ex5_tuning_reg_parameter.m
@@ -1,68 +1,78 @@
 clear all
 clc
-
+dim = 10;
 max_iteration_size = 200;
-j  = -6 : 7; % from 10^?6 up to 10^3
+j  = -6 : 3; % from 10^?6 up to 10^3
 % j = 10^-6 : 10^3; % for longer iterations
-disp(numel(j))
+% disp(numel(j))
 % initialize things:
 [mse_test_100, mse_test_10] = deal(zeros(max_iteration_size, numel(j)));
 [mse_train_100, mse_train_10] = deal(zeros(max_iteration_size, numel(j)));
 [mse_valid_100, mse_valid_10] = deal(zeros(max_iteration_size, numel(j)));
 
 for idx = 1:numel(j)
-    gamma=10^j(idx);
+    gamma(idx) = 10^j(idx);
     for i=1:max_iteration_size
         %Size 100 training and 10 size training; Size 500 test 0.8-t 0.2-v;
-        [mse_train_100(i,idx), mse_valid_100(i,idx), mse_test_100(i,idx)] = get_mean_square_error_with_split(100,500,gamma,0.8,0.2);
-        [mse_train_10(i,idx), mse_valid_10(i,idx), mse_test_10(i,idx)] = get_mean_square_error_with_split(10,500,gamma,0.8,0.2);
+        [X_600, y_600]= generate_data(600, dim);
+        X_test = X_600(1:500,:);
+        y_test = y_600(1:500,:);
+        X_train_100 = X_600(501:600,:);
+        y_train_100 = y_600(501:600,:);
+        X_train_10 = X_600(501:510,:);
+        y_train_10 = y_600(501:510,:);
+        [mse_train_100(i,idx), mse_valid_100(i,idx), mse_test_100(i,idx)] = get_mean_square_error_with_split(X_train_100,y_train_100,X_test,y_test,gamma(idx),0.8,0.2);
+        [mse_train_10(i,idx), mse_valid_10(i,idx), mse_test_10(i,idx)] = get_mean_square_error_with_split(X_train_10,y_train_10,X_test,y_test,gamma(idx),0.8,0.2);
     end   
 end
 
 % plotting graphs
 figure
-% create array with gamma values 10^-6, 10^-5 and so on..
-for idx = 1:numel(j)
-    gamma(idx)=10^j(idx);
-end
-disp(gamma)
+
+% disp(gamma)
 
 % min gamma accross average errors of 200 iterations
 % a)
-mse_train_100_avr = mean(mse_train_100(:,:),1); %# average along dim 1, i.e. average all rows
-mse_valid_100_avr = mean(mse_valid_100(:,:),1);
-[min_valid_error,min_valid_error_index] = min(mse_valid_100_avr);
-min_gamma = gamma(min_valid_error_index);
-fprintf('100: min_gamma=%d min_valid_error=%d\n',min_gamma,min_valid_error);
-mse_test_100_avr = mean(mse_test_100(:,:),1); 
-semilogx(gamma, mse_train_100_avr, '--r', gamma, mse_valid_100_avr, 'b', gamma, mse_test_100_avr, 'g')
+% mse_train_100_avr = mean(mse_train_100); %# average along dim 1, i.e. average all rows
+% mse_valid_100_avr = mean(mse_valid_100);
+% [min_valid_error,min_valid_error_index] = min(mse_valid_100_avr);
+% min_gamma = gamma(min_valid_error_index);
+% fprintf('100: min_gamma=%d min_valid_error=%d\n',min_gamma,min_valid_error);
+% mse_test_100_avr = mean(mse_test_100(:,:),1); 
+% semilogx(gamma, mse_train_100_avr, '--r', gamma, mse_valid_100_avr, 'g', gamma, mse_test_100_avr, 'b')
+% legend({'--r','g','b'}, {'training data set (80 samples)','validation data set (20 samples)','test data set (500 samples)'})
+% title('Question 5.a) Plot of the validation error, together with training and test error 200 iterations')
+% xlabel('log scale of the regularization parameter')
+% ylabel('mean square error')
+% grid on
 
 % b)
-% mse_train_10_avr = mean(mse_train_10(:,:),1); 
-% mse_valid_10_avr =  mean(mse_valid_10(:,:),1); 
-% [min_valid_error_10,min_valid_error_index_10] = min(mse_valid_10_avr);
-% min_gamma_10 = gamma(min_valid_error_index_10);
-% fprintf('10: min_gamma=%d min_valid_error=%d\n',min_gamma_10,min_valid_error_10);
-% mse_test_10_avr = mean(mse_test_10(:,:),1); 
-% semilogx(gamma, mse_train_10_avr, '--r', gamma, mse_valid_10_avr, 'b', gamma, mse_test_10_avr, 'g')
+mse_train_10_avr = mean(mse_train_10); 
+mse_valid_10_avr =  mean(mse_valid_10); 
+[min_valid_error_10,min_valid_error_index_10] = min(mse_valid_10_avr);
+min_gamma_10 = gamma(min_valid_error_index_10);
+fprintf('10: min_gamma=%d min_valid_error=%d\n',min_gamma_10,min_valid_error_10);
+mse_test_10_avr = mean(mse_test_10); 
+semilogx(gamma, mse_train_10_avr, '--r', gamma, mse_valid_10_avr, 'b', gamma, mse_test_10_avr, 'g')
+legend({'--r','g','b'}, {'training data set (8 samples)','validation data set (2 samples)','test data set (500 samples)'})
+title('Question 5.b) Plot of the validation error, together with training and test error 200 iterations')
+xlabel('log scale of the regularization parameter')
+ylabel('mean square error')
+grid on
 
 % c) 
 % average min gamma accross iterations
-[gamma_100, gamma_10] = deal(zeros(max_iteration_size));
-for i=1:max_iteration_size
-    mse_valid_10_iter = mse_valid_10(i,:);
-    [min_valid_error_iter_10, min_valid_error_iter_10_index] = min(mse_valid_10_iter);
-    gamma_10(i)=gamma(min_valid_error_iter_10_index);
-    
-    mse_valid_100_iter = mse_valid_100(i,:);
-    [min_valid_error_iter_100, min_valid_error_iter_100_index] = min(mse_valid_100_iter);
-    gamma_100(i)=gamma(min_valid_error_iter_100_index);
-end
-gamma_10_avr = mean(gamma_10);
-gamma_100_avr = mean(gamma_100);
-fprintf('gamma_10_avr=%d\ngamma_100_avr=%d\n',gamma_10_avr,gamma_100_avr);
+% for i=1:max_iteration_size
+%     mse_valid_10_iter = mse_valid_10(i,:);
+%     [min_valid_error_iter_10, min_valid_error_iter_10_index] = min(mse_valid_10_iter);
+%     gamma_10(i) = gamma(min_valid_error_iter_10_index);
+%     
+%     mse_valid_100_iter = mse_valid_100(i,:);
+%     [min_valid_error_iter_100, min_valid_error_iter_100_index] = min(mse_valid_100_iter);
+%     gamma_100(i) = gamma(min_valid_error_iter_100_index);
+% end
+% gamma_10_avr = mean(gamma_10);
+% gamma_100_avr = mean(gamma_100);
+% fprintf('gamma_10_avr=%d\ngamma_100_avr=%d\n',gamma_10_avr,gamma_100_avr);
 
-title('Question 5')
-xlabel('gamma')
-ylabel('mean square error')
 
--- get_mean_square_error_with_split.m
+++ get_mean_square_error_with_split.m
@@ -1,31 +1,38 @@
-function [ mse_train, mse_valid, mse_test] = get_mean_square_error_with_split( i_train_size, i_test_size, gamma, split_train, split_valid )
-    d = 10; %Dimension
-    I = eye(d); % Identity matrix
+function [ mse_train, mse_valid, mse_test] = get_mean_square_error_with_split( Xtr, y_tr, Xte, y_test, gamma, split_train, split_valid ) 
+    len_te = size(Xte, 1);
+    len_train = size(Xtr, 1)*split_train;
+    len_valid = size(Xtr, 1)*split_valid;
+%     fprintf('len_te=%d len_train=%d len_valid=%d\n',len_te,len_train,len_valid);
     
-    i_valid_size = i_train_size * split_valid;
-    i_new_train_size = i_train_size * split_train;
-
-    Xtr=randn(i_new_train_size, d);   %X is drawn from the standard normal distribution.
-    n_train=randn(i_new_train_size,1);   %n is drawn from the standard normal distribution. 
-    w_train=randn(d,1); % weighted vector= random weight vector w ? R10 
-    y_train=Xtr*w_train+n_train; %Calculate gamma for training data  
-    l_train=i_new_train_size; 
-    wtr_est = (Xtr'*Xtr + gamma*l_train*I) \ Xtr'*y_train;
-    mse_train = 1/l_train*((Xtr*wtr_est-y_train)'*(Xtr*wtr_est-y_train)); 
-    
-    mse_valid = get_mse(i_valid_size, d, wtr_est);
-    mse_test = get_mse(i_test_size, d, wtr_est);
+%     Dividing training set into training data set and validation training
+%     set with split_train/split_valid ratio
+    X_train = Xtr(1:len_train,:);
+    X_valid = Xtr(len_train+1:len_train+len_valid,:);
+    y_train = y_tr(1:len_train,:);
+    y_valid = y_tr(len_train+1:len_train+len_valid,:);
+%     fprintf('size(X_train, 1)=%d size(X_valid, 1)=%d size(y_train, 1)=%d size(y_valid, 1)=%d\n',size(X_train, 1),size(X_valid, 1),size(y_train, 1), size(y_valid, 1));
+       
+    dim = size(X_train, 2);
+    wtr_est = ((X_train' * X_train) + (gamma*len_train*eye(dim))) \ (X_train'*y_train);
+ 
+    mse_train = get_mse(X_train, y_train, wtr_est, len_train);
+    mse_valid = get_mse(X_valid, y_valid, wtr_est, len_valid);
+    mse_test = get_mse(Xte, y_test, wtr_est, len_te);
    %     fprintf('function mse_train = %d mse_test = %d mse_valid = %d\n',mse_train,mse_test,mse_valid);
 end
 
-function [mse] = get_mse (i_size, d, wtr_est)
-    X=randn(i_size, d);   %X is drawn from the standard normal distribution.    
-    n=randn(i_size,1);   %n is drawn from the standard normal distribution.
-    w=randn(d,1); % weighted vector= random weight vector w ? R10
-    y=X*w+n; %Calculate gamma for training data 
-    l=i_size;    
-    % Using equation (3) compute the mean squared error on both the training
-    % and test sets.
-    mse = 1/l*((X*wtr_est-y)'*(X*wtr_est-y)); %Mean square error for the test data is way higher
+function mse = get_mse(X, Y, w, len)
+    mse = (1.0/len) * (w' * (X)' * X * w - 2 * Y' * X * w + Y' * Y);
 end
+
+% function [mse] = get_mse (i_size, d, wtr_est)
+%     X=randn(i_size, d);   %X is drawn from the standard normal distribution.    
+%     n=randn(i_size,1);   %n is drawn from the standard normal distribution.
+%     w=randn(d,1); % weighted vector= random weight vector w ? R10
+%     y=X*w+n; %Calculate gamma for training data 
+%     l=i_size;    
+%     % Using equation (3) compute the mean squared error on both the training
+%     % and test sets.
+%     mse = 1/l*((X*wtr_est-y)'*(X*wtr_est-y)); %Mean square error for the test data is way higher
+% end
 
