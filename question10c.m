208dcd8986dfe40b18ac0f62e86a70f81ff55307
Джон <kolesnik-vlad@mail.ru>
2016-01-14 13:23:03 +0000

    q10c) K_train and K_test have correct dimensions now.

--- question10c.m
+++ question10c.m
@@ -14,14 +14,19 @@ for i = 1:numel(j)
     gamma(i) = 10^j(i);
     
     for ii = 1:numel(jj)
-        sigma = 10^jj(i);
+        sigma = 10^jj(ii);
         
         sizeBoston=size(bostonArray,1);
         BostonArrayRand=bostonArray(randperm(sizeBoston),:);
 
         K = get_kernel(BostonArrayRand, sigma);
         fprintf('size(K,1)=%d\n',size(K,1));
+        kernel_size = size(K,1);
+        train_size = floor(sizeBoston*2/3);
+        K_train = K(1:train_size, 1:train_size);
+        K_test = K(train_size+1:kernel_size, 1:train_size);
         
+        fprintf('K_train=%dx%d K_test=%dx%d\n',size(K_train,1), size(K_train,2), size(K_test,1), size(K_test,2));
 %         K_train = get_kernel(TestBoston, TrainingBoston, sigma);
 %         K_test = get_kernel(TestBoston, TestBoston, sigma);
 %         
