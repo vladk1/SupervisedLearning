8449f144594c52d4a47080cc0d2874200eac3976
Джон <kolesnik-vlad@mail.ru>
2016-01-15 05:07:42 +0000

    q4) Looks good.

--- ex4_ridgeRegression.m
+++ ex4_ridgeRegression.m
@@ -1,12 +1,12 @@
 clear all
 clc
 % Perform ridge regression on the data sets as generated in exercise 2 part a 
-% for ? ranging from 10^?6 up to 10^3, and all powers of 10 in between. 
+% for y ranging from 10^-6 up to 10^3, and all powers of 10 in between. 
 % What are the training and test mean squared errors? 
-% Plot the training and test set errors as a function of ? (use a log scale for the ? axis).
+% Plot the training and test set errors as a function of y.
 
 max_iteration_size = 200;
-j  = -6 : 3; % from 10^?6 up to 10^3
+j  = -6 : 3; % from 10^-6 up to 10^3
 
 [mse_test_100, mse_test_10] = deal(zeros(max_iteration_size, numel(j)));
 [mse_train_100, mse_train_10] = deal(zeros(max_iteration_size, numel(j)));
@@ -24,42 +24,37 @@ for idx = 1:numel(j)
         X_train_10 = X_600(501:510,:);
         y_train_10 = y_600(501:510,:);
         [mse_train_10(i,idx), mse_test_10(i,idx)] = get_mean_square_error(X_train_10,y_train_10,X_test,y_test,gamma(idx));  
-%         fprintf('gamma=%d mse_train=%d mse_test=%d\n',gamma(idx), mse_train_100(i,idx),mse_test_100(i,idx));
     end   
 end
 
 % plotting graphs
-figure
-
 % a)
 % 1 for first iteration
-% disp(mse_train_100(1,:))
-% disp(mse_test_100(1,:))
-% semilogx(gamma, mse_train_100(1,:),'r',gamma, mse_test_100(1,:), 'b')
-% title('Question 4.a) training data size = 100')
-% xlabel('log scale of the regularization parameter')
-% ylabel('mean square error')
-% legend({'r','b'}, {'training data set (100 samples)','test data set (500 samples)'})
+figure
+semilogx(gamma, mse_train_100(1,:),'r',gamma, mse_test_100(1,:), 'b')
+title('Question 4.a) training data size = 100')
+xlabel('log scale of the regularization parameter')
+ylabel('mean square error')
+legend({'r','b'}, {'training data set (100 samples)','test data set (500 samples)'})
+
 % b)
-% disp(mse_train_10(1,:))
-% disp(mse_test_10(1,:))
-% semilogx(gamma, mse_train_10(1,:), 'r', gamma, mse_test_10(1,:), 'b')
-% title('Question 4.b) training data size = 10')
-% xlabel('log scale of the regularization parameter')
-% ylabel('mean square error')
-% legend({'r','b'}, {'training data set (10 samples)','test data set (500 samples)'})
+figure
+semilogx(gamma, mse_train_10(1,:), 'r', gamma, mse_test_10(1,:), 'b')
+title('Question 4.b) training data size = 10')
+xlabel('log scale of the regularization parameter')
+ylabel('mean square error')
+legend({'r','b'}, {'training data set (10 samples)','test data set (500 samples)'})
+
 % c)
 % average erros accross 200 iterations
-% disp(mse_train_100_avr)
-% disp(mse_test_100_avr) 
+figure
 semilogx(gamma, mean(mse_train_100), 'r',gamma, mean(mse_test_100), 'b')
-% title('Question 4.c) Averaged (200 runs) MSE for training data (100 samples) and test data sets')
-% xlabel('log scale of the regularization parameter')
-% ylabel('mean square error')
-% legend({'r','b'}, {'training data set (100 samples)','test data set (500 samples)'})
+title('Question 4.c) Averaged (200 runs) MSE for training data (100 samples) and test data sets')
+xlabel('log scale of the regularization parameter')
+ylabel('mean square error')
+legend({'r','b'}, {'training data set (100 samples)','test data set (500 samples)'})
 
-% disp(mse_train_10_avr)
-% disp(mse_test_10_avr) 
+figure
 semilogx(gamma, mean(mse_train_10), 'r',gamma, mean(mse_test_10), 'b')
 grid on
 title('Question 4.c) Averaged (200 runs) MSE for training data (10 samples) and test data sets')
