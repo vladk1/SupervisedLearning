bb17324fce5a2845123ac7b053e7f392da12b39e
Джон <kolesnik-vlad@mail.ru>
2016-01-15 04:59:54 +0000

    q10) esthetical corrections.

--- kridgereg.m
+++ kridgereg.m
@@ -1,8 +1,6 @@
 function [ dual_w ] = kridgereg( K, y, gamma )
 % The function returns vector alpha of the dual weights
-%  Takes in kernel matrix, y and gamma.
-dim = size(K,2);
-len = size(K,1);
+% Takes in kernel matrix, y and gamma.
 dual_w = (K + gamma * size(K,1) * eye(size(K,1))) \ y;
 end
 
